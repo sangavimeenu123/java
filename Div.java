2161d19854d992eb70ab1a10b9cd03ab3c6ad492
manojsudharsan <manojsudharsanbe@gmail.com>
2019-02-22 10:56:55 +0530

    Update Div.java

--- Div.java
+++ Div.java
@@ -8,11 +8,18 @@ Scanner sc=new Scanner(System.in);
 int a=sc.nextInt();
 int b=sc.nextInt();
 int c=0;
+if(a>b)
+{
 while(a>=b)
 {
 a=a-b;
 c=c+1;
 }
 System.out.println(c);
+}
+else
+{
+System.out.println("MAX_INT");
+}
 }
 }
