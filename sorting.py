91f6b54007f4e7b226ca06603edbed5ce064e961
manojsudharsan <manojsudharsanbe@gmail.com>
2019-06-04 14:36:21 +0530

    Create sorting.py

---
+++ sorting.py
@@ -0,0 +1,10 @@
+N=int(input())
+if N>=1 and N<=100000:
+                      t=[]
+                      for i in range(N):
+                            b=int(input())
+                            t.append(b)
+                      t.sort()
+                      print(t)
+else:
+     exit
