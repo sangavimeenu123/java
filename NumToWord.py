9b062c2d524f7d2b1e1de71f86346cbd17ee7424
manojsudharsan <manojsudharsanbe@gmail.com>
2019-06-07 09:24:58 +0530

    Create NumToWord.py

---
+++ NumToWord.py
@@ -0,0 +1,21 @@
+X=int(input())
+if X==1:
+    print("One")
+elif X==2:
+    print("Two")
+elif X==3:
+    print("Three")
+elif X==4:
+    print("Four")
+elif X==5:
+    print("Five")
+elif X==6:
+    print("Six")
+elif X==7:
+    print("Seven")
+elif X==8:
+    print("Eight")
+elif X==9:
+    print("Nine")
+elif X==10:
+    print("Ten")
